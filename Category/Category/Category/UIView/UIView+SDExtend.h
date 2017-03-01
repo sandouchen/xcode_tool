179339c38bd681d7011ae5cef71b6c3a1dbf7348
fqq3 <fqq3@fqq3deiMac.local>
2017-03-01 12:55:16 +0800

    增加UIView动画分类

--- Category/Category/Category/UIView/UIView+SDExtend.h
+++ Category/Category/Category/UIView/UIView+SDExtend.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+float radiansForDegrees(int degrees);
+
 @interface UIView (SDExtend)
 /** 控件头部位置*/
 @property(nonatomic, assign) CGFloat sd_top;
@@ -40,4 +42,110 @@
  *  添加边框&颜色:四边
  */
 - (void)setBorder:(UIColor *)color width:(CGFloat)width;
+
+
+//***************************UIView 动画*********************************
+/**
+ *   位移
+ */
+- (void)moveTo:(CGPoint)destination duration:(float)secs option:(UIViewAnimationOptions)option;
+
+/**
+ *   位移
+ */
+- (void)moveTo:(CGPoint)destination duration:(float)secs option:(UIViewAnimationOptions)option delegate:(id)delegate callback:(SEL)method;
+
+/**
+ *   位移
+ */
+- (void)raceTo:(CGPoint)destination withSnapBack:(BOOL)withSnapBack;
+
+/**
+ *   位移
+ */
+- (void)raceTo:(CGPoint)destination withSnapBack:(BOOL)withSnapBack delegate:(id)delegate callback:(SEL)method;
+
+/**
+ *   旋转
+ */
+- (void)rotate:(int)degrees secs:(float)secs delegate:(id)delegate callback:(SEL)method;
+
+/**
+ *  缩放
+ */
+- (void)scale:(float)secs x:(float)scaleX y:(float)scaleY delegate:(id)delegate callback:(SEL)method;
+
+/**
+ *  顺时针旋转
+ *
+ *  @param secs 动画执行时间
+ */
+- (void)spinClockwise:(float)secs;
+
+/**
+ *  逆时针旋转
+ *
+ *  @param secs 动画执行时间
+ */
+- (void)spinCounterClockwise:(float)secs;
+
+/**
+ *  顺时针旋转一次
+ *
+ *  @param secs 动画执行时间
+ */
+- (void)spinClockwiseOnce:(float)secs;
+
+/**
+ *  逆时针旋转一次
+ *
+ *  @param secs 动画执行时间
+ */
+- (void)spinCounterClockwiseOnce:(float)secs;
+
+/**
+ *  反翻页效果
+ *
+ *  @param secs 动画执行时间
+ */
+- (void)curlDown:(float)secs;
+
+/**
+ *  视图翻页后消失
+ *
+ *  @param secs 动画执行时间
+ */
+- (void)curlUpAndAway:(float)secs;
+
+/**
+ *  旋转缩放到一点后消失
+ *
+ *  @param secs 动画执行时间
+ */
+- (void)drainAway:(float)secs;
+
+/**
+ *  将视图改变到一定透明度
+ *
+ *  @param newAlpha alpha
+ *  @param secs     动画执行时间
+ */
+- (void)changeAlpha:(float)newAlpha secs:(float)secs;
+
+
+/**
+ *  改变透明度结束动画后还原(闪烁动画)
+ *
+ *  @param secs         alpha
+ *  @param continuously 是否循环
+ */
+- (void)pulse:(float)secs continuously:(BOOL)continuously;
+
+
+/**
+ *  以渐变方式添加子控件
+ *
+ *  @param subview 需要添加的子控件
+ */
+- (void)addSubviewWithFadeAnimation:(UIView *)subview;
 @end
