1e4c631c890b7a7a92955599fb8ca5f695d49e66
sandouchen <77315628@qq.com>
2017-06-22 12:04:53 +0800

    6.22 更新SDCycleScrollView

--- Category/Pods/Target Support Files/SDCycleScrollView/SDCycleScrollView-umbrella.h
+++ Category/Pods/Target Support Files/SDCycleScrollView/SDCycleScrollView-umbrella.h
@@ -10,14 +10,13 @@
 #endif
 #endif
 
-#import "NSData+SDDataCache.h"
-#import "SDCollectionViewCell.h"
-#import "SDCycleScrollView.h"
-#import "UIView+SDExtension.h"
 #import "TAAbstractDotView.h"
 #import "TAAnimatedDotView.h"
 #import "TADotView.h"
 #import "TAPageControl.h"
+#import "SDCollectionViewCell.h"
+#import "SDCycleScrollView.h"
+#import "UIView+SDExtension.h"
 
 FOUNDATION_EXPORT double SDCycleScrollViewVersionNumber;
 FOUNDATION_EXPORT const unsigned char SDCycleScrollViewVersionString[];
